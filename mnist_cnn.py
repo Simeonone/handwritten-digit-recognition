7958452d80f7df62934de6d6fc0793dc94c39c58
Simeonone <simeon.kengere@gmail.com>
2024-06-02 19:24:16 +0300

    initial commit

---
+++ mnist_cnn.py
@@ -0,0 +1,52 @@
+import tensorflow as tf
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Load the MNIST dataset
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# Normalize the images to the range [0, 1]
+x_train = x_train.astype('float32') / 255.0
+x_test = x_test.astype('float32') / 255.0
+
+# Reshape the data to include the channel dimension (since the images are grayscale)
+x_train = x_train.reshape(-1, 28, 28, 1)
+x_test = x_test.reshape(-1, 28, 28, 1)
+
+# Convert the labels to one-hot encoding
+y_train = to_categorical(y_train, 10)
+y_test = to_categorical(y_test, 10)
+
+# Build the model
+model = Sequential([
+    Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(28, 28, 1)),
+    MaxPooling2D(pool_size=(2, 2)),
+    Conv2D(64, kernel_size=(3, 3), activation='relu'),
+    MaxPooling2D(pool_size=(2, 2)),
+    Flatten(),
+    Dense(128, activation='relu'),
+    Dense(10, activation='softmax')
+])
+
+# Compile the model
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Train the model
+model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=5)
+
+# Evaluate the model
+test_loss, test_accuracy = model.evaluate(x_test, y_test)
+print(f'Test accuracy: {test_accuracy}')
+
+# Predict the first 10 images from the test set
+predictions = model.predict(x_test[:10])
+
+# Plot the images and their predicted labels
+for i in range(10):
+    plt.imshow(x_test[i].reshape(28, 28), cmap='gray')
+    plt.title(f'Predicted Label: {np.argmax(predictions[i])}')
+    plt.show()
